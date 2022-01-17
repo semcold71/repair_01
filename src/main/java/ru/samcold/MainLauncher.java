2c4d846b7aa6098067f483e3df2bdb7c291bf513
semcold71 <semcold@gmail.com>
2022-01-17 18:26:48 +0300

    Initial

---
+++ src/main/java/ru/samcold/MainLauncher.java
@@ -0,0 +1,7 @@
+package ru.samcold;
+
+public class MainLauncher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
---
+++ src/main/java/ru/samcold/controllers/MainController.java
@@ -0,0 +1,4 @@
+package ru.samcold.controllers;
+
+public class MainController {
+}
